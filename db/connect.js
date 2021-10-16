c764035fec339f72a809e619d06c12c55bd42d1c
diljot-1 <68597974+diljot-1@users.noreply.github.com>
2021-10-16 14:44:00 +0530

    Update connect.js
    secret key hidden

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dotenv=require('dotenv');
 dotenv.config();
-mongoose.connect( "mongodb+srv://diljot_1:dj123987@cluster0.brfki.mongodb.net/testenginedb?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: false}, (err)=>{
+mongoose.connect( "",{useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: false}, (err)=>{
     if(err)
       console.log(err);
     else
